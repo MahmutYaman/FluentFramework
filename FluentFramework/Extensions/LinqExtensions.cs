c678304daf239ff1799196be01bac251dbad78f4
Mahmut Yaman <mahmutyamann@gmail.com>
2019-09-17 14:37:30 +0300

    Replaced shuffling alogirthm with a better one.

--- FluentFramework/Extensions/LinqExtensions.cs
+++ FluentFramework/Extensions/LinqExtensions.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace FluentFramework.Extensions
 {
     public static class LinqExtensions
     {
-        public static T Random<T>(this IEnumerable<T> source)
+        public static void Shuffle<T>(this IList<T> list)
         {
-            return source.ElementAt(new Random().Next(0, source.Count()));
-        }
-
-        public static T RandomOrDefault<T>(this IEnumerable<T> source)
-        {
-            var count = source.Count();
-            return count == 0 ? default : source.ElementAt(new Random().Next(0, count));
-        }
-
-        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
-        {
-            var random = new Random();
-            return source.OrderBy(x => random.Next());
+            using (var provider = new RNGCryptoServiceProvider())
+            {
+                var n = list.Count;
+                while (n > 1)
+                {
+                    var box = new byte[1];
+                    do provider.GetBytes(box);
+                    while (!(box[0] < n * (byte.MaxValue / n)));
+                    var k = (box[0] % n);
+                    n--;
+                    T value = list[k];
+                    list[k] = list[n];
+                    list[n] = value;
+                }
+            }
         }
     }
 }
